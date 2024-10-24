517622b111e58b97dd81fe563a90ae5666d4950d
addreeh <adrianpinohidalgo@gmail.com>
2024-10-24 12:21:27 +0200

    switch to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
+// import vercel from '@astrojs/vercel/serverless'
+
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: cloudflare()
 })
